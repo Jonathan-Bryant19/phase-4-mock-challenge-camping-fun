eea526ef7d15e9d3f3e76978c505f0eafb2757c2
Jonathan Bryant <spectrum19tech@gmail.com>
2022-03-19 11:46:23 -0700

    commit tree ed5e02a0897e62660377fc35faac1102c21ff829

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,17 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
 
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+  rescue_from ActiveRecord::RecordInvalid, with: :not_valid
+
+  private
+
+  def not_found(not_found)
+    render json: { error: "#{not_found.model} not found" }, status: :not_found
+  end
+
+  def not_valid(invalid)
+    render json: { errors: [invalid.record.errors.full_messages] }, status: 422
+  end
+
 end
